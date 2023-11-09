a3f64c3d3300f2c72c923131d75dc45d54f9f0f8
minTwin <michaelmin2020@gmail.com>
2023-11-09 06:52:24 -0800

    update

--- product.py
+++ product.py
@@ -1,5 +1,5 @@
 class Product:
-    def __init__(self, name, interest_rate=None):  # The interest_rate defaults to None because no rate has been set yet.
-        self.name = name                           # This accounts for the default interest rate being set in the json file, else it would have
-        self.interest_rate = interest_rate         # to be hardcoded in the rule_engine file
+    def __init__(self, name, interest_rate=0):  
+        self.name = name                           
+        self.interest_rate = interest_rate         
         self.disqualified = False
