8d1163db2674823a99749281b82b50602f376b78
minTwin <michaelmin2020@gmail.com>
2023-11-24 08:32:55 -0600

    updated test methods

--- test_rules_engine.py
+++ test_rules_engine.py
@@ -27,42 +27,42 @@ class TestRulesEngine(unittest.TestCase):
         engine = RulesEngine(self.rules)
         person = Person(720, 'Florida')
         product = Product('7-1 ARM')
-        engine.run_rules(person, product)
+        engine.apply_rules(person, product)
         self.assertTrue(product.disqualified, "Product should be disqualified for the state of Florida")
 
     def test_non_disqualified_state(self):
         engine = RulesEngine(self.rules)
         person = Person(720, 'Illinois')
         product = Product('7-1 ARM')
-        engine.run_rules(person, product)
+        engine.apply_rules(person, product)
         self.assertFalse(product.disqualified, "Product should not be disqualified for the state of Illinois")
 
     def test_interest_rate_adjustment_for_credit_score_of_720(self):
         engine = RulesEngine(self.rules)
         person = Person(720, 'Florida')
         product = Product('7-1 ARM')
-        engine.run_rules(person, product)
+        engine.apply_rules(person, product)
         self.assertEqual(product.interest_rate, 5.2, "Interest rate should be adjusted to 5.2 for credit score of 720")
 
     def test_interest_rate_adjustment_for_credit_score_over_720(self):
         engine = RulesEngine(self.rules)
         person = Person(730, 'Florida')
         product = Product('7-1 ARM')
-        engine.run_rules(person, product)
+        engine.apply_rules(person, product)
         self.assertEqual(product.interest_rate, 5.2, "Interest rate should be adjusted to 5.2 for credit score of 730")
 
     def test_interest_rate_adjustment_for_credit_score_below_720(self):
         engine = RulesEngine(self.rules)
         person = Person(710, 'Florida')
         product = Product('7-1 ARM')
-        engine.run_rules(person, product)
+        engine.apply_rules(person, product)
         self.assertEqual(product.interest_rate, 6.0, "Interest rate should be adjusted to 6.0 for credit score of 710")
 
     def test_alternative_product(self):
         engine = RulesEngine(self.rules)
         person = Person(720, 'Texas')
         product = Product('7-2 ARM')
-        engine.run_rules(person, product)
+        engine.apply_rules(person, product)
         self.assertEqual(product.interest_rate, 5.3, "Interest rate should be adjusted to 5.3 for product 7-2 ARM")       
 
 # If this file is run directly, run the tests
