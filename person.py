2a30a768957bdece3082e882589c7006bf4ed642
minTwin <michaelmin2020@gmail.com>
2023-11-28 11:32:41 -0600

    added comments

--- person.py
+++ person.py
@@ -1,5 +1,5 @@
 
 class Person:
-    def __init__(self, credit_score, state):
+    def __init__(self, credit_score, state): #constructor that initialize attributes for the Person object
         self.credit_score = credit_score
         self.state = state
